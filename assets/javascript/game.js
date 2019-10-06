36f60536a97d1f0aa0e9c1af50f2c62d19e25b4f
windsor80 <windsorjunior@hotmail.com>
2019-10-06 04:13:32 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,8 @@
 // Global Variables
 
 // Array with all possible cities to be chosen
-var cities = ["beijing", "london", "moscow", "orlando", "paris", "rome", "sydney", "tokyo", "amsterdam", "athens", "barcelona", "berlin", "brasilia", "chicago", "dublin", "havana", "hiroshima", "istanbul", "liverpool", "madrid", "miami", "montreal", "prague", "seoul", "windsor", "munich", "toronto", "manchester", "melbourne", "wellington"];
+var cities = ["beijing", "london", "moscow", "orlando", "paris", "rome", "sydney", "tokyo", "amsterdam", "athens", "barcelona", "berlin", "brasilia", "chicago", "dublin", "havana", "hiroshima", "istanbul", "liverpool", "madrid", "miami", "montreal", "prague", "seoul", "windsor", "munich", "toronto", "manchester", "melbourne", "wellington", "santiago", "medellin", "lisbon", "cairo", "montevideo"];
 
-// munich - toronto - manchester - melbourne - wellington
-// santiago - medellin - lisbon - cairo - montevideo
 // bangkok - warsaw - brussels - venice - zurich - osaka
 
 // Variable to store the chosen city to be played in each round
